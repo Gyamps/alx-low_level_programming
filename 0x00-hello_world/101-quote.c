20f2d1e09478bbdc682d2ab98d28f59655ccb94b
Gyamps <dentukwabena.kg@gmail.com>
2023-02-16 15:34:35 -0800

    add 101 script

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Print exact string followed by new line to standard error
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
