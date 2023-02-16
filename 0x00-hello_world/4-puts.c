5a597736f9958611dd863cfa153a61962804acc3
Gyamps <dentukwabena.kg@gmail.com>
2023-02-16 14:51:32 -0800

    add 4 script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Program prints string to screen with puts function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
