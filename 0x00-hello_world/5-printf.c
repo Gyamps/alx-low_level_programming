27d3b7d542f68a70f19f212cef8ae342df2b9baf
Gyamps <dentukwabena.kg@gmail.com>
2023-02-16 14:57:16 -0800

    add 5 script

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** main - Entry point of program
+ *
+ * Program prints a String using printf function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the utcome is a piece of art,\n");
+	return (0);
+}
