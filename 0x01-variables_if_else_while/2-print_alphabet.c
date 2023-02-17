54e50a9b8270e44e03d969b114a6dce72263d2bf
Gyamps <dentukwabena.kg@gmail.com>
2023-02-17 12:56:58 -0800

    add 2 script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n=97; n<=122; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
