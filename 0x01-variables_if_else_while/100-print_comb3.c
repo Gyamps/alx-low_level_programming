9943b0a7eb2391ef275ae671936309458d4c3a39
Gyamps <dentukwabena.kg@gmail.com>
2023-02-17 15:41:23 -0800

    fix 100 script

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,15 +14,10 @@ int main(void)
 	{
 		for (j = i + 1; j <= 57; j++)
 		{
-			if (i == 56 && j == 57)
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
 			{
-				putchar(i);
-				putchar(j);
-			}
-			else
-			{
-				putchar(i);
-				putchar(j);
 				putchar(44);
 				putchar(32);
 			}
