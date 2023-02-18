77e64e0603a2b0f8106ce4a2ef403dab0c8d721f
Gyamps <dentukwabena.kg@gmail.com>
2023-02-17 17:09:29 -0800

    add 102 script

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 1; k <= 57; k++)
+			{
+				for (l = j + 1; l <= 57; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(l);
+					if ((i == 57 & j == 56) && (k == 57 &&  (k == 57 && l == 57))
+							continue;
+					else
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
+}
