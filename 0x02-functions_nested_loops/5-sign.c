6fcd4e92a7db276146c0c50f7acc8924c89f6387
Gyamps <dentukwabena.kg@gmail.com>
2023-02-21 08:36:27 -0800

    fix return error

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		return (49);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		return (48);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		return (47);
 	}
 	_putchar('\n');
 }
