3b70d4be759558419b6b8887b3e4a996615b9f50
Gyamps <dentukwabena.kg@gmail.com>
2023-02-21 06:54:02 -0800

    fix 3 error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,8 +11,10 @@
 
 int _islower(int c)
 {
-	if ((c <= 'a') && (c <= 'z'))
+	if ((c >= 'a') && (c <= 'z'))
 		return (1);
-	else if ((c <= 'A') && (c <= 'Z'))
+	else if ((c >= 'A') && (c <= 'Z'))
 		return (0);
+
+	return (0);
 }
