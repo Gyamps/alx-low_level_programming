8e27b2498e91c05c55608516c318e27267239905
Gyamps <dentukwabena.kg@gmail.com>
2023-02-22 13:38:41 -0800

    add 3 script

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -28,4 +28,31 @@ void positive_or_negative(int i);
  */
 int largest_number(int a, int b, int c);
 
+/**
+ * print_remaining_days - converts a date to the day of year and determines
+ * how many days are left in the year, taking leap year into consideration
+ *
+ * Function takes 3 integer arguments
+ *
+ * @month: First integer args (month in number format)
+ * @day: Second integer args (day of month)
+ * @year: Third integer args (year)
+ *
+ * Return: nothing
+ */
+void print_remaining_days(int month, int day, int year);
+
+/**
+ * convert-day - converts day of month to day of year, without accounting
+ * for leap year
+ *
+ * Function takes 2 integer arguments
+ *
+ * @month: First int args (month in number format)
+ * @day: Second integer args (day of month)
+ *
+ * Return: day of year
+ */
+int convert_day(int month, int day);
+
 #endif
