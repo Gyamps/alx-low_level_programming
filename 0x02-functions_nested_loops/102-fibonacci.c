d34d954e1c161e4b125b8b47dbc460116c903d59
Gyamps <dentukwabena.kg@gmail.com>
2023-02-21 19:16:02 -0800

    add 102 script

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 50 fibonacci numbers, starting iwth 1 and 2
+ *
+ * Function has no arguments
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long int n, x, f_num;
+
+	n = 1;
+	x = 2;
+	printf("%ld, %ld, ", n, x);
+	for (i = 1; i <= 50; i++)
+	{
+		f_num = n + x;
+		if (i == 50)
+			printf("%ld\n", f_num);
+		else
+			printf("%ld, ", f_num);
+
+		n = x;
+		x = f_num;
+	}
+	return (0);
+}
