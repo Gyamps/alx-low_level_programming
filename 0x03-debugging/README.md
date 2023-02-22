0b5e46340621dfa07d7f7db76bec0c459073d0ad
Gyamps <dentukwabena.kg@gmail.com>
2023-02-22 12:59:29 -0800

    addheader file and 0 script

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,4 @@
+## Tasks
+### 0-main.c
+> Debug code to test for integer input of 0
+
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * positive_or_negative - function prints out if integer is positive or
+ * negative
+ *
+ * Function requires 1 int argument
+ *
+ * @i: The integer argument
+ *
+ * Return: nothing
+ */
+void positive_or_negative(int i);
+
+#endif
