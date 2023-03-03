84ddcb4544c9e6d08df72e057f018ed2a9134aae
Gyamps <dentukwabena.kg@gmail.com>
2023-03-02 19:04:39 -0800

    add 6 script

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ *
+ * @s: the string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	int i;
+
+	for (i = 0; *s; i++)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @s: the string
+ *
+ * Return: the string
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0l i < _strlen(s); i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] -= 32;
+		}
+	}
+	return (str);
+}
