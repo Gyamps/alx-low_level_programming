39b5a36fb3acc9d57d12302de4326e2a53f3bf44
Gyamps <dentukwabena.kg@gmail.com>
2023-03-22 18:36:56 -0700

    fix closing brace

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	printf("%d\n", op_func(atoi(argv[1], atoi(argv[3])));
+	printf("%d\n", op_func(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
