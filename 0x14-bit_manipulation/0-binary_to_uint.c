d012e69e589cc7f896e6d22b59fe52b83eb56164
Gyamps <dentukwabena.kg@gmail.com>
2023-08-19 05:42:32 -0700

    updated the code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,68 +1,32 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-
-int _strlen(const char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- * _pow_recursion - calculate exponenet
- * @x: base
- * @y: exponenet
- *
- * Return: x exponent y
- */
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-
-	if (x == 0)
-		return (0);
-
-	if (x == 1 || y == 0)
-		return (1);
-
-	return (x * _pow_recursion(x, y - 1));
-}
-
-/**
  * binary_to_uint - convert binary to unsigned int
  * @b: binary
  * Return: unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i, len_b, digit;
-	unsigned int sum = 0, j = 0;
-
-	len_b = _strlen(b) - 1;
+	int len_b = 0, i, j;
+	unsigned int sum = 0;
 
 	if (b == NULL)
-		return (0);
+		return (sum);
 
-	for (i = len_b; i >= 0; i++)
+	for (i = 0; b[i]; i++)
+		len_b++;
+	len_b -= 1;
+
+	j = 0;
+	while (b[j])
 	{
-		digit = b[i] - '0';
-		if (digit != 0 && digit != 1)
-			return (0);
+		if ((b[j] != '0') && (b[i] != '1'))
+			return (sum);
 
-		sum += digit * _pow_recursion(2, j);
+		if (b[i] == '1')
+			sum += (1 * (1 << len_b));
 		j++;
+		len_b--;
 	}
 
 	return (sum);
